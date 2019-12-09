a5db57499a93ecc29f37e9776e19b87c37472e8b
Bill Fan <1472451850qq.com>
2019-12-09 17:22:17 +0800

    [WIP] cleaning up UI and adding additional sliders

--- SimulationTest.py
+++ SimulationTest.py
@@ -1,28 +1,28 @@
 # A simple kalman filter for a constant acceleration model, no input
+# The model used for the system transform function is simply based on the constant acceleration data
+# Therefore this model is not applicable to situations with non-constant acceleration
 
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 from matplotlib.widgets import Slider
 
-ENTRIES = 100
-BASEVALUE = 1000
-NOISE = BASEVALUE - 1
+ENTRIES = 50
+BASEVALUE = 30
+NOISE = BASEVALUE - 10
 SEED = 200000
-RATEOFCHANGE = 1
-DELTA_T = 1
+RATEOFCHANGE = 0.01 # = Acceleration / 2 (in m/s^2?)
+DELTA_T = 3
 
 np.random.seed(SEED)
 
 def generateDataSet():
     rawData = []
     for i in range(ENTRIES):
-        rawData.append(BASEVALUE + RATEOFCHANGE * i**2)
+        rawData.append(BASEVALUE + RATEOFCHANGE * (i * DELTA_T)**2)
     noisyData = rawData + np.random.randint(-NOISE, NOISE, ENTRIES)
     return rawData, noisyData
 
-
-    
 rawNumbers, noisyNumbers = generateDataSet()
 
 print(noisyNumbers)
@@ -33,30 +33,34 @@ for i in range(ENTRIES):
 
 figure, ax = plt.subplots()
 plt.subplots_adjust(left = 0.25, bottom = 0.25)
+plt.ylim(0,noisyNumbers[ENTRIES - 1] * 1.1)
+plt.ylabel("Distance (m)")
+plt.xlabel("Time (s)")
 
 truth_plot, = plt.plot(counterArray, rawNumbers, color = 'black', linewidth = "2", zorder = 0)
 measurement_plot, = plt.plot(counterArray, noisyNumbers, zorder = 10, linewidth = "2")
 filtered_plot, = plt.plot(counterArray, counterArray, color = 'red', zorder = 5, linewidth = "2")
 
-plt.ylim(0,BASEVALUE + NOISE + (RATEOFCHANGE * (ENTRIES**2)) + BASEVALUE/10)
-
-base_xhat = 10
+# Initialization of base values
+base_xhat = BASEVALUE / 2
 base_kalmanGain = 0
 base_errorCovariance = 2 * (NOISE**2)
 base_measurementCovariance = NOISE**2
 
 # Adding the slider axis graphically, setting their positions and color
 axcolor = "lightgoldenrodyellow"
-axXhat = plt.axes([0.25, 0.05, 0.65, 0.03], facecolor = axcolor)
-aSeed = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor = axcolor)
-axErrorCovariance = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor = axcolor)
-axMeasurementCovariance = plt.axes([0.25, 0.2, 0.65, 0.03], facecolor = axcolor)
+axXhat = plt.axes([0.3, 0.25, 0.5, 0.03], facecolor = axcolor)
+aSeed = plt.axes([0.3, 0.2, 0.5, 0.03], facecolor = axcolor)
+axErrorCovariance = plt.axes([0.3, 0.15, 0.5, 0.03], facecolor = axcolor)
+axMeasurementCovariance = plt.axes([0.3, 0.1, 0.5, 0.03], facecolor = axcolor)
+axNoise = plt.axes([0.3, 0.05, 0.5, 0.03], facecolor = axcolor)
 
 # Creating the slider widgets on top of the axis
-sXhat = Slider(axXhat, 'xhat', 0.1, 2 * BASEVALUE, valinit = base_xhat)
+sXhat = Slider(axXhat, 'init Xhat', 0.1, 2 * BASEVALUE, valinit = base_xhat)
 sSeed = Slider(aSeed, 'Seed', 0, SEED, valinit = SEED, valstep=1)
-sErrorCovariance = Slider(axErrorCovariance, 'Error Covariance', 0.1, 2 * base_errorCovariance, valinit = base_errorCovariance)
-sMeasurementCovariance = Slider(axMeasurementCovariance, 'MeasurementCovariance', 0.1, 2 * base_measurementCovariance, valinit = base_measurementCovariance)
+sErrorCovariance = Slider(axErrorCovariance, 'Init Error Covariance', 0.1, 2 * base_errorCovariance, valinit = base_errorCovariance)
+sMeasurementCovariance = Slider(axMeasurementCovariance, 'Measurement Covariance', 0.1, 2 * base_measurementCovariance, valinit = base_measurementCovariance)
+sNoise = Slider(axNoise, 'Noise', 1, BASEVALUE - 1, valinit = NOISE)
 
 # Actual Kalman Filter calculations for 1d static
 def filter(xhat, seed, errorCovariance, measurementCovariance):
@@ -94,10 +98,16 @@ def filter(xhat, seed, errorCovariance, measurementCovariance):
 def sliderOnChanged(val):
     filter(sXhat.val, sSeed.val, sErrorCovariance.val, sMeasurementCovariance.val)    
 
+def noiseOnChanged(val):
+    global NOISE
+    NOISE = val
+    sliderOnChanged(val) 
+
 sXhat.on_changed(sliderOnChanged)
 sSeed.on_changed(sliderOnChanged)
 sErrorCovariance.on_changed(sliderOnChanged)
 sMeasurementCovariance.on_changed(sliderOnChanged)
+sNoise.on_changed(noiseOnChanged)
 
 filter(base_xhat, SEED, base_errorCovariance, base_measurementCovariance)
 
